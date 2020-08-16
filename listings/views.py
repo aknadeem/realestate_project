672a09f84a4f86bd246e2ed74491361d6d99af55
nadeem <nadeem@vappar.com>
2020-08-16 23:29:11 +0500

    create and setup postgresql databse and apply migration

---
+++ listings/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+	return render(request, 'listing/listings.html')
+
+def listing(request):
+	return render(request, 'listing/listing.html')
+
+def search(request):
+	# return HttpResponse('search')
+	return render(request, 'listing/search.html')
