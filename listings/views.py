b2597242d9f383fed622448554a3389e1c29f23b
nadeem <nadeem@vappar.com>
2020-08-18 19:00:38 +0500

    display data in front end and also add pagination

--- listings/views.py
+++ listings/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
-from django.http import HttpResponse
+from .models import Listing
 
 # Create your views here.
 
 def index(request):
-	return render(request, 'listing/listings.html')
+	listings = Listing.objects.all()
 
-def listing(request):
+	paginator = Paginator(listings, 6)
+	page = request.GET.get('page')
+	paged_listings = paginator.get_page(page)	
+
+	context = { 
+		'listings' : paged_listings 
+	}
+
+	return render(request, 'listing/listings.html', context)
+
+def listing(request, listing_id):
 	return render(request, 'listing/listing.html')
 
 def search(request):
